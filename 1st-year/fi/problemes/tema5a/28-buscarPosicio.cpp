27ed106f7cb3044543ffff51d5d9cfc78a5a0193
Hugo GB <hugogaben8.02@gmail.com>
2020-11-04 22:57:06 +0100

    update

--- 1st-year/fi/problemes/tema5a/28-buscarPosicio.cpp
+++ 1st-year/fi/problemes/tema5a/28-buscarPosicio.cpp
@@ -6,28 +6,35 @@ using namespace std;
 
 #define SIZE 5
 
-int main(){
+int main()
+{
   int arr[SIZE], OrdDecreixent, BuscarPos, numBuscarPos;
 
   LlegirVector(arr, SIZE);
 
   OrdDecreixent = OrdenatDecreixent(arr, SIZE);
 
-  switch (OrdDecreixent) {
-    case 0:
-      cout << "El vector no esta correctament ordenat" << endl;
-      break;
-    case 1:
-      cout << "Introdueix un valor: "; cin >> numBuscarPos;
+  switch (OrdDecreixent)
+  {
+  case 0:
+    cout << "El vector no esta correctament ordenat" << endl;
+    break;
+  case 1:
+    cout << "Introdueix un valor: ";
+    cin >> numBuscarPos;
 
-      BuscarPos = BuscarPosicio(arr, numBuscarPos, SIZE);
+    BuscarPos = BuscarPosicio(arr, numBuscarPos, SIZE);
 
-      if (BuscarPos == -1) {
-	cout << "Tots els valors del vector son mes grans que " << numBuscarPos << endl;	
-      } else {
-	cout << "El valor ha d'anar a la posicio " << BuscarPos << endl;
-      }
-  } 
+    if (BuscarPos == -1)
+    {
+      cout << "Tots els valors del vector son mes grans que " << numBuscarPos << endl;
+    }
+    else
+    {
+      cout << "El valor ha d'anar a la posicio " << BuscarPos << endl;
+    }
+    break;
+  }
 
   return 0;
 }
