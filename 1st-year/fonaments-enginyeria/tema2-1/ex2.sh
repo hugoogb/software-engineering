f0c2bf494209c6417c95fe4ce6a83b0a7e0cf11a
Hugoo GB <hugogaben8.02@gmail.com>
2020-11-18 23:58:21 +0100

    added some script exercises and some c++ exercises

--- 1st-year/fonaments-enginyeria/tema2-1/ex2.sh
+++ 1st-year/fonaments-enginyeria/tema2-1/ex2.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-camp=`head -1 esportistes.txt | cut -d: -f1`
-echo "${camp}"
+linia=$1
+fitxer=$2
 
-echo "Introdueix el nou camp: \c"
-read campCanviat
+let liniaAnterior=linia-1
+let liniaSeguent=linia+1
 
-# echo ${campCanviat} >> esportistes.txt
+`tail +$liniaSeguent $fitxer > part3`
+
+if [ $linia -gt 1 ]
+then
+  `head -$liniaAnterior $fitxer > part1`
+  `cat part1 >> $fitxer`
+fi
+
+`head -$linia $fitxer | tail -1 | cut -d: -f2- > linia2_2`
+
+read -p "Introdueix el nou camp: " campCanviat
+echo "$campCanviat" > linia2_1
+
+`paste -d: linia2_1 linia2_2 > part2`
+
+cat /dev/null > esportistes.txt
+
+`cat part2 >> $fitxer`
+`cat part3 >> $fitxer`
