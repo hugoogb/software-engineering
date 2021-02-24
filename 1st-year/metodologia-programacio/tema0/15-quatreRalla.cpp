5460a70040c852a5f09c2871a76a7af869988c3e
Hugoo GB <hugogaben8.02@gmail.com>
2021-02-24 11:40:43 +0100

    added new subject

---
+++ 1st-year/metodologia-programacio/tema0/15-quatreRalla.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+const int N_FILES = 6;
+const int N_COLUMNES = 7;
+
+int tauler[N_FILES][N_COLUMNES];
+
+int posaFitxa(int tauler[N_FILES][N_COLUMNES], int columna, int jugador)
+{
+}
